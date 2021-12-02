6ecf6af8ee998a2d187b3cd8a0e6ac05b265392f
Raviraj <60103458+Ravirajayya@users.noreply.github.com>
2021-12-02 23:08:24 +0530

    Delete kafka-practice directory
    wrong file uploaded

--- kafka-practice/src/main/java/com/kafka/kafkapractice/common/AppConstant.java
+++
@@ -1,7 +0,0 @@
-package com.kafka.kafkapractice.common;
-
-public class AppConstant {
-
-	public static final String TOPIC_NAME_USER_LOG = "user-log";
-	public static final String GROUP_ID = "group-id";
-}
